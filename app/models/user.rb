312045fd5d0758840ab1f909446d752e6b3e0377
Rachitan Nichita Mihai <nichita@192-168-0-105.rdsnet.ro>
2019-07-15 17:42:49 +0300

    Authorization, Admins, users and pagination

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 244},
                     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, length: {minimum: 6}, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string.
   def User.digest(string)
